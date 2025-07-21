f263b479660b70df24a196232feddac2337eb3bf
Pablo Martín <goinnn@gmail.com>
2025-07-21 12:39:47 +0200

    Ready to new release

--- rest_framework_jwt/__init__.py
+++ rest_framework_jwt/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'djangorestframework-jwt'
-__version__ = '1.12.2'
+__version__ = '1.13.0'
 __author__ = 'José Padilla'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2014-2017 Blimp LLC'
